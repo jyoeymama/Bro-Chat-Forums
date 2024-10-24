fbb8e0153bf00d7a148cbb036964c828b836365a
jyo mama <jyomama28@gmail.com>
2024-10-24 17:17:02 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYLnK1UNF1-LecZ3IeyL6ElkdFtQQxUXE",
+  authDomain: "bro-chat-forums.firebaseapp.com",
+  projectId: "bro-chat-forums",
+  storageBucket: "bro-chat-forums.appspot.com",
+  messagingSenderId: "289665705294",
+  appId: "1:289665705294:web:5d7dc7cc578a7161a26a1f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
